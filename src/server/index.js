bdb5057ea1c2121ef2c4abbeb07b95eb66254934
Juan.Ixcopal <juan.ixcopal@alumnos.uneatlantico.es>
2022-12-23 12:29:57 +0100

    Feature: cambios en el server

--- src/server/index.js
+++ src/server/index.js
@@ -7,11 +7,11 @@ let _express = null
 const { SERVER_PORT, SERVER_HOST } = config
 
 const start = () => {
-    _express = express().use(router({ config }))
-    return new Promise(resolve => {
-        _express.listen(SERVER_PORT, SERVER_HOST, console.log('Server running on port:', SERVER_PORT))
-        resolve()
-    })
+  _express = express().use(router({ config }))
+  return new Promise(resolve => {
+    _express.listen(SERVER_PORT, SERVER_HOST, console.log('Server running on port:', SERVER_PORT))
+    resolve()
+  })
 }
 
 start()
